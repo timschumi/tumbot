4712264df16810cba6b6660914285307a872418f
Frank <frank@elsinga.de>
2019-11-28 12:14:45 +0100

    added excellence and trivial autocomplete messages. Now Just the first Part has to be added to the .txt file. Also renamed the quotes.py file and merged the excellenz.py file into the new UserGeneratedContentFile

---
+++ cogs/UserGeneratedContent.py
@@ -0,0 +1,36 @@
+import random
+
+from discord.ext import commands
+
+
+class Quotes(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def quote(self, ctx):
+        await ctx.send(random.choice(open("UserGeneratedContent/quotes.txt").readlines()))
+
+
+class Exzellenz(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def exzellenz(self, ctx):
+        await ctx.send(random.choice(
+            random.choice(open("UserGeneratedContent/excellence_noAutocompletion.txt").readlines()),
+            self.excellence_formating(self),
+            self.trivial_formating(self)))
+
+    def excellence_formating(self):
+        return "%s ist sehr exzellent.", random.choice(
+            open("UserGeneratedContent/exzellenz_autocomplete.txt").readlines())
+
+    def trivial_formating(self):
+        return "%s ist trivial.", random.choice(open("UserGeneratedContent/exzellenz_autocomplete.txt").readlines())
+
+
+def setup(bot):
+    bot.add_cog(Quotes(bot))
+    bot.add_cog(Exzellenz(bot))
