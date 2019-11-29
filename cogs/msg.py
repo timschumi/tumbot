ae08d6912db0fcabcede18dd5f39f35ef2e194ac
Tim Schumacher <tim.schumacher@tum.de>
2019-11-29 19:05:15 +0100

    msg: Search through cleaned content

--- cogs/msg.py
+++ cogs/msg.py
@@ -25,7 +25,7 @@ class MessageStore(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        search = re.search(r'#(\w+)', message.content)
+        search = re.search(r'#(\w+)', message.clean_content)
         if search is None:
             return
 
