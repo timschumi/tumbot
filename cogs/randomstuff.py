e07189bffa37e226ec40616826aa40660791bc69
Tim Schumacher <tim.schumacher@tum.de>
2019-11-29 17:55:15 +0100

    Introduce a randomstring() Method

--- cogs/randomstuff.py
+++ cogs/randomstuff.py
@@ -6,23 +6,25 @@ class Randomstuff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def randomstring(self, file):
+        return random.choice(open(f"strings/{file}.txt").readlines())
+
     @commands.command()
     async def quote(self, ctx):
-        await ctx.send(random.choice(open("strings/quotes.txt").readlines()))
+        await ctx.send(self.randomstring("quotes"))
 
     @commands.command()
     async def exzellenz(self, ctx):
         await ctx.send(random.choice(
-            random.choice(open("strings/excellence_noAutocompletion.txt").readlines()),
+            random.choice(self.randomstring("exzellence_noAutocompletion")),
             self.excellence_formating(),
             self.trivial_formating()))
 
     def excellence_formating(self):
-        return "%s ist sehr exzellent.", random.choice(
-            open("strings/exzellenz_autocomplete.txt").readlines())
+        return "%s ist sehr exzellent.", self.randomstring("exzellenz_autocomplete"))
 
     def trivial_formating(self):
-        return "%s ist trivial.", random.choice(open("strings/exzellenz_autocomplete.txt").readlines())
+        return "%s ist trivial.", self.randomstring("exzellenz_autocomplete"))
 
 
 def setup(bot):
