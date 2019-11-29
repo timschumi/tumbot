64353dd8d7c4faeef98ee5eccbf189f1e4614d50
Tim Schumacher <tim.schumacher@tum.de>
2019-11-29 17:48:08 +0100

    Merge UserGeneratedContent classes into Randomstuff

--- cogs/randomstuff.py
+++ cogs/randomstuff.py
@@ -2,19 +2,13 @@ import random
 
 from discord.ext import commands
 
-
-class Quotes(commands.Cog):
+class Randomstuff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def quote(self, ctx):
         await ctx.send(random.choice(open("strings/quotes.txt").readlines()))
-
-
-class Exzellenz(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
 
     @commands.command()
     async def exzellenz(self, ctx):
@@ -32,5 +26,4 @@ class Exzellenz(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(Quotes(bot))
-    bot.add_cog(Exzellenz(bot))
+    bot.add_cog(Randomstuff(bot))
