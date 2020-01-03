b53d0340c006c4d5c33b9fd1cbbc112ebc97fbb9
Tim Schumacher <timschumi@gmx.de>
2020-01-03 21:17:56 +0100

    Revert "music: Nuke play and playlocal commands"
    This reverts commit d068d36c0044c88a7ef46555ebdaf33092022f68.

--- cogs/music.py
+++ cogs/music.py
@@ -66,8 +66,27 @@ class Music(commands.Cog):
         await channel.connect()
 
     @commands.command()
+    async def playlocal(self, ctx, *, query):
+        """Plays a file from the local filesystem"""
+
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send('Now playing: {}'.format(query))
+
+    @commands.command()
     async def play(self, ctx, *, url):
-        """Plays things from URL (or search)"""
+        """Plays from a url (almost anything youtube_dl supports)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send('Now playing: {}'.format(player.title))
+
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        """Streams from a url (same as yt, but doesn't predownload)"""
 
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
@@ -91,7 +110,9 @@ class Music(commands.Cog):
 
         await ctx.voice_client.disconnect()
 
+    @playlocal.before_invoke
     @play.before_invoke
+    @stream.before_invoke
     async def ensure_voice(self, ctx):
         if ctx.voice_client is None:
             if ctx.author.voice:
