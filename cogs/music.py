2d58694a51aa442409be1dc00140a9bcf0e0667b
Tim Schumacher <timschumi@gmx.de>
2020-01-04 12:40:04 +0100

    Move ytdl temporary files to seperate directory

--- cogs/music.py
+++ cogs/music.py
@@ -11,7 +11,7 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'outtmpl': '.ytdl-cache/%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
