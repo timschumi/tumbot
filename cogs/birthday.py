aaeb45c20a408ab72ed7ab3c71d8867a7c69998f
Tim Schumacher <timschumi@gmx.de>
2020-03-07 17:37:37 +0100

    Resolve remaining issues
    - Save target channel for each server
    - Loop through servers by connection
    - Add a command to change saved target channel
    - Simplify query logic

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -1,7 +1,8 @@
 import datetime
 import re
 from typing import Pattern
-
+import asyncio
+import discord
 from discord.ext import commands
 
 
@@ -12,82 +13,113 @@ class Birthdays(commands.Cog):
                                            r"(30\.((0?[13-9])|(1[0-2]))\.)"  # all months with 30 days
                                            r"|"
                                            r"(31\.((0?[13578])|(10)|(12))\.)")  # all months with 31 days
-    BIRTHDAY_CHANNEL_ID = 666653781366145028
-
     def __init__(self, bot):
         self.bot = bot
-        self.bot.register_job(60 * 60 * 24, self.congratulate)
+        self.bot.register_job(60 * 60 * 24, self.congratulate_all)
 
     @commands.group(aliases=['birth', 'birthday', 'birthdate', 'geburtstag'], invoke_without_command=True)
     async def birthdays(self, ctx):
-        """manage birthdays"""
+        """Manages birthdays"""
         pass
 
     @birthdays.command()
-    @commands.has_permissions(manage_messages=True)
-    async def list(self, ctx, querry=""):
-        """lists all birthdays (birthdays or userid are possible, jet optional querries)"""
-        with self.bot.db.get(self.BIRTHDAY_CHANNEL_ID) as db:
-            if len(querry) is 0:  # No Querry
+    async def list(self, ctx, query=""):
+        """Lists all birthdays
+
+        `query` is either a date or a User-ID.
+        """
+        with self.bot.db.get(ctx.guild.id) as db:
+            if len(query) == 0:  # No Query
                 results = db.execute("SELECT userId, day, month FROM birthdays").fetchall()
-                text = ""
-                for result in results:
-                    text += "User: {}\t->\t{}.{}.\n".format(result[0], result[1], result[2])
-                await ctx.send(text)
-            elif self.DATEPATTERN.match(querry) is not None:  # Birthday as Querry
-                day, month = querry.strip(".").split(".")
+            elif self.DATEPATTERN.match(query) is not None:  # Birthday as Query
+                day, month = query.strip(".").split(".")
                 results = db.execute(
                     "SELECT userId, day, month FROM birthdays WHERE day = ? AND month = ?", (day, month)).fetchall()
-                if len(results) is 0:
-                    await ctx.send("No Birthday at {}.{}.".format(day, month))
-                    return
-                text = ""
-                for result in results:
-                    text += "User: {}\t->\t{}.{}.\n".format(result[0], result[1], result[2])
-                await ctx.send(text)
-            else:  # Username as Querry
+            else:  # Username as Query
                 results = db.execute(
-                    "SELECT userId, day, month FROM birthdays WHERE userId LIKE '?'", querry).fetchall()
-                print(results)
-                if len(results) is 0:
-                    await ctx.send("No UserID matches this request")
-                    return
-                await ctx.send("User: {}\t->\t{}.{}.\n".format(results[0][0], results[0][1], results[0][2]))
+                    "SELECT userId, day, month FROM birthdays WHERE userId LIKE ?", (query,)).fetchall()
+
+        if len(results) == 0:
+            await ctx.send("No entries found.")
+            return
+
+        text = ""
+        for result in results:
+            user = await self.bot.fetch_user(result[0])
+            text += "User: {}\t->\t{}.{}.\n".format(user.name, result[1], result[2])
+        await ctx.send(text)
 
     @birthdays.command()
     async def add(self, ctx, birthdate):
-        """adds a new bithday <DD.MM.> to the database, if possible"""
+        """Adds a birthday (DD.MM.) for the calling user"""
         if self.DATEPATTERN.match(birthdate) is None:
             await ctx.send("Usage: <!birthday add DD.MM.> (of course with a valid date)")
             return
         day, month = birthdate.strip(".").split(".")
-        self.update_birthday(ctx.message.author.id, day, month)
+
+        # Update message channel if needed
+        if self.bot.dbconf_get(ctx.guild.id, "birthday_channel") is None:
+            self.bot.dbconf_set(ctx.guild.id, "birthday_channel", ctx.channel.id)
+
+        with self.bot.db.get(ctx.guild.id) as db:
+            db.execute("INSERT OR REPLACE INTO birthdays (userId, day, month) VALUES (?, ?, ?)",
+                (ctx.author.id, day, month))
+
         await ctx.message.add_reaction('\U00002705')
+
+    @birthdays.command()
+    @commands.has_permissions(manage_messages=True)
+    async def channel(self, ctx, target: discord.TextChannel = None):
+        """Sets a new output channel for birthday messages"""
+        if target is not None:
+            self.bot.dbconf_set(ctx.guild.id, "birthday_channel", target.id)
+            await ctx.message.add_reaction('\U00002705')
+            return
+
+        channel_id = self.bot.dbconf_get(ctx.guild.id, "birthday_channel")
+
+        if channel_id is None:
+            await ctx.send("No channel set!")
+            return
+
+        channel = await self.bot.fetch_channel(channel_id)
+
+        if channel is None:
+            await ctx.send(f"Could not resolve saved channel ({channel_id}).")
+            return
+
+        await ctx.send(f"Current birthday channel: {channel.mention}")
+        return
 
     def get_current_date(self) -> [int, int]:
         date = datetime.datetime.now()
         return date.day, date.month
 
-    def update_birthday(self, user_id, day, month):
-        with self.bot.db.get(self.BIRTHDAY_CHANNEL_ID) as db:
-            db.execute("INSERT OR REPLACE INTO birthdays (userId, day, month) VALUES ('?', ?, ?)", (
-                user_id, day, month))
-
-    async def congratulate(self):
+    def congratulate_all(self):
         day, month = self.get_current_date()
+        for conn in self.bot.db.get_all():
+            asyncio.run_coroutine_threadsafe(self.congratulate(conn, day, month), self.bot.loop).result()
+
+    async def congratulate(self, conn, day, month):
         text = "Geburtstage am {}.{}.:".format(day, month)
-        users = self.get_user_ids(day, month)
+        users = conn.execute(
+                "SELECT userId FROM birthdays WHERE day = ? AND month = ?", (day, month)).fetchall()
         if len(users) == 0:
             return
-        for user in self.get_user_ids(day, month):
-            text += ":tada: :fireworks: :partying_face: **Alles Gute zum Geburtstag**, <@!{}>" \
-                    " :partying_face: :fireworks: :tada:\n".format(user)
-        await self.bot.fetch_channel(self.BIRTHDAY_CHANNEL_ID).send(text)
+        for user in users:
+            text += "\n    :tada: :fireworks: :partying_face: **Alles Gute zum Geburtstag**, <@{}>" \
+                    " :partying_face: :fireworks: :tada:".format(user[0])
 
-    def get_user_ids(self, day: int, month: int) -> [int]:
-        with self.bot.db.get(self.BIRTHDAY_CHANNEL_ID) as db:
-            return db.execute(
-                "SELECT userId FROM birthdays WHERE day = ? AND month = ?", (day, month)).fetchall()
+        # This is essentially what dbconf_get does, but we don't have the guild ID :/
+        channel = conn.execute("SELECT value FROM config WHERE name = 'birthday_channel'").fetchall()
+
+        # If channel isn't set
+        if len(channel) < 1:
+            return
+
+        channel = await self.bot.fetch_channel(channel[0][0])
+
+        await channel.send(text)
 
 
 def setup(bot):
