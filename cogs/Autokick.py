900adf8b62392508e893c89e306b2bc3cdf944dd
Kroko-fant <schrottsuperdeluxe@gmail.com>
2020-03-27 00:27:28 +0100

    Minor changes

---
+++ cogs/Autokick.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+
+class Autokick(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.blocks = []
+
+    @commands.command
+    @commands.has_permissions(administrator=True)
+    async def add(self, user):
+        try:
+            self.blocks.append(int(user))
+        except Exception:
+            pass
+
+    @commands.command
+    @commands.has_permissions(administrator=True)
+    async def remove(self, user):
+        try:
+            self.blocks.remove(int(user))
+        except Exception:
+            pass
+
+    @commands.Cog.listener
+    async def on_voice_state_update(self, member, before, after):
+        if member.id in self.blocks:
+            await member.move_to(None)
+
+
+def setup(client):
+    client.add_cog(Autokick(client))
