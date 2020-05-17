237828028a5a7a45a892e1b62a59cec26211896e
Kroko-fant <schrottsuperdeluxe@gmail.com>
2020-05-18 00:21:39 +0200

    Der Bot kann nun DM-Nachrichten senden und empfangen

--- cogs/johannes.py
+++ cogs/johannes.py
@@ -5,6 +5,7 @@ class Johannes(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.sendchannel = 711702521793740852
 
     def cheatsheet(self, subject):
         return f"Ja wir dürfen in der {subject}-Klausur ein Cheat-Sheet verwenden. Dieses ist ein doppelseitig " \
@@ -19,6 +20,24 @@ class Johannes(commands.Cog):
         if message.author.bot:
             return
         lower = message.content.lower()
+
+        if message.guild is None and not message.author.bot:
+            channel = self.bot.get_channel(self.sendchannel)
+            content = f'**{message.author}** sagt: "{message.content}"'
+            if len(message.content) < 1800:
+                await channel.send(content)
+            else:
+                await channel.send(content[0:1800])
+                await channel.send(content[1801])
+            return
+
+        if (message.channel.id == self.sendchannel) and message.content[0:18].isnumeric():
+            await message.add_reaction("✅")
+            dmchannel = self.bot.get_user(int(message.content[0:18]))
+            if dmchannel.dm_channel is None:
+                await dmchannel.create_dm()
+            await dmchannel.dm_channel.send(message.content[19:])
+            return
 
         # Reactions
         if "johannes" in lower or "stöhr" in lower:
