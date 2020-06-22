696da541d33ab3dc8b9fb68013eda19572cc5fb9
Tim Schumacher <timschumi@gmx.de>
2020-06-22 03:16:47 +0200

    dbmgr: Inline pattern strings

--- dbmgr.py
+++ dbmgr.py
@@ -5,9 +5,6 @@ import sqlite3
 
 
 class DbMgr:
-    DB_PATTERN = 'db/{}.db'
-    SCHEMA_PATTERN = 'db/schema_{}.sql'
-
     def __init__(self):
         self.db_handles = {}
 
@@ -36,15 +33,15 @@ class DbMgr:
         return list(self.db_handles.values())
 
     def create_new_conn(self, guild):
-        connection = sqlite3.connect(self.DB_PATTERN.format(guild), check_same_thread=False)
+        connection = sqlite3.connect(f"db/{guild}.db", check_same_thread=False)
         connection.row_factory = sqlite3.Row
         return connection
 
     def upgrade_db(self, connection):
         user_version = connection.execute("PRAGMA user_version").fetchone()[0]
 
-        while os.path.isfile(self.SCHEMA_PATTERN.format(user_version + 1)):
-            with open(self.SCHEMA_PATTERN.format(user_version + 1)) as file:
+        while os.path.isfile(f"db/schema_{user_version + 1}.sql"):
+            with open(f"db/schema_{user_version + 1}.sql") as file:
                 connection.executescript(file.read())
             user_version = connection.execute("PRAGMA user_version").fetchone()[0]
 
