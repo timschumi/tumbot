6d9ac769fcd878212885cfcad33dad083e83d573
Tim Schumacher <timschumi@gmx.de>
2020-10-03 18:29:34 +0200

    confmgr: Rename guild_id to dbid

--- basedbot/confmgr.py
+++ basedbot/confmgr.py
@@ -2,14 +2,14 @@ class ConfigManager:
     def __init__(self, db):
         self.db = db
 
-    def get(self, guild_id, name, default=None):
-        result = self.db.get(guild_id).execute("SELECT value FROM config WHERE name = ?", (name,)).fetchall()
+    def get(self, dbid, name, default=None):
+        result = self.db.get(dbid).execute("SELECT value FROM config WHERE name = ?", (name,)).fetchall()
 
         if len(result) < 1:
             return default
 
         return str(result[0][0])
 
-    def set(self, guild_id, name, value):
-        with self.db.get(guild_id) as db:
+    def set(self, dbid, name, value):
+        with self.db.get(dbid) as db:
             db.execute("REPLACE INTO config (name, value) VALUES (?, ?)", (name, value))
