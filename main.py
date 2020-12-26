c65a3f0d12ed1fea4f137d32f277ceb08d0a52c2
Tim Schumacher <timschumi@gmx.de>
2020-12-26 23:36:07 +0100

    sql: Move guild schemas to different folder

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ bot.conf.register('prefix', default='!', description="The command prefix that th
 bot.add_cog_path('cogs')
 bot.add_cog_path('cogs/legacy')
 
-bot.db.add_sql_path('sql')
+bot.db.add_sql_path('sql/guild', scope='guild')
 
 # Load all modules
 for cog in bot.find_all_cogs():
