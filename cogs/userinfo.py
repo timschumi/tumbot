b356af13f658caaeda770fd30e29c214e9410308
Tim Schumacher <timschumi@gmx.de>
2022-03-31 15:48:32 +0200

    userinfo: Limit role listing to a length of 1024

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -2,6 +2,20 @@ import discord
 from discord.ext import commands
 
 
+def _join(delimiter, values, maxlen):
+    joined = ""
+
+    for value in values:
+        string = (delimiter if len(joined) > 0 else "") + str(value)
+
+        if len(joined) + len(string) > maxlen:
+            return joined
+
+        joined += string
+
+    return joined
+
+
 class Userinfo(commands.Cog):
     # pylint: disable=missing-class-docstring
 
@@ -32,7 +46,7 @@ class Userinfo(commands.Cog):
                                 value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
         userinfoembed.add_field(name='Beigetreten:', value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
 
-        userinfoembed.add_field(name=f'Rollen ({len(roles)})', value="\n".join([role.mention for role in roles]))
+        userinfoembed.add_field(name=f'Rollen ({len(roles)})', value=_join("\n", [role.mention for role in roles], 1024))
         userinfoembed.add_field(name='Höchste Rolle:', value=str(member.top_role.mention))
 
         userinfoembed.add_field(name='Bot?', value=str(member.bot))
