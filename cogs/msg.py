4de778fb69ba80a08949cc7a10def712652d5c95
Philipp <xarantolus@protonmail.com>
2022-11-06 10:24:28 +0100

    msg: Allow captioning images via memegen (#72)

--- cogs/msg.py
+++ cogs/msg.py
@@ -1,7 +1,39 @@
 import re
+from urllib.parse import urlparse, quote
 from discord.ext import commands
 
 import basedbot
+
+
+def _is_url(text):
+    try:
+        result = urlparse(text)
+        return all([result.scheme, result.netloc, result.path])
+    except ValueError:
+        return False
+
+def _memegen_escape_text(text):
+    # according to https://memegen.link/#special-characters
+    escapes = [
+        ("_", "__"),
+        ("-", "--"),
+        (" ", "_"),
+        ("\n", "~n"),
+        ("?", "~q"),
+        ("&", "~a"),
+        ("%", "~p"),
+        ("#", "~h"),
+        ("/", "~s"),
+        ("\\", "~b"),
+        ("<", "~l"),
+        (">", "~g"),
+        ("\"", "''"),
+    ]
+
+    for char, escape in escapes:
+        text = text.replace(char, escape)
+
+    return text
 
 
 class MessageStore(commands.Cog):
@@ -37,12 +69,75 @@ class MessageStore(commands.Cog):
         content = await commands.clean_content().convert(ctx, content)
 
         with self.bot.db.get(ctx.guild.id) as db:
-            if len(db.execute("SELECT name, content FROM msg WHERE name = ?", (name.lower(),)).fetchall()) > 0:
-                db.execute("UPDATE msg SET content = ? WHERE name = ?", (content, name.lower()))
+            current_msg = db.execute(
+                "SELECT name, content, allow_memegen FROM msg WHERE name = ?", (name.lower(),)).fetchall()
+            if len(current_msg) > 0:
+                # only allow memegen if the current message is still an URL
+                keep_memegen = current_msg[0][2] and _is_url(content)
+                db.execute("UPDATE msg SET content = ?, allow_memegen = ? WHERE name = ?",
+                           (content, keep_memegen, name.lower()))
             else:
                 db.execute("INSERT INTO msg (name, content) VALUES (?, ?)", (name.lower(), content))
 
         await ctx.message.add_reaction('\U00002705')
+
+    @msg.command()
+    @basedbot.has_permissions("msg.set")
+    async def memegen(self, ctx, action, shorthand):
+        """Enable or disable meme generation for a shorthand"""
+
+        if action not in ("enable", "disable"):
+            await ctx.send("Invalid action. Valid actions are `enable` and `disable`")
+            return
+
+        with self.bot.db.get(ctx.guild.id) as db:
+            result = db.execute("SELECT name, content FROM msg WHERE name = ? OR name = ?",
+                                (shorthand.lower(), "-" + shorthand.lower())).fetchall()
+
+        if len(result) <= 0:
+            await ctx.send("Shorthand not found.")
+            return
+
+        shorthand_text = result[0][1]
+        if not _is_url(shorthand_text):
+            await ctx.send("Shorthand text must be a valid URL to manage meme generation.")
+            return
+
+        with self.bot.db.get(ctx.guild.id) as db:
+            result = db.execute("UPDATE msg SET allow_memegen = ? WHERE name = ? OR name = ?", (
+                action == "enable", shorthand.lower(), "-" + shorthand.lower()))
+
+        await ctx.message.add_reaction('\U00002705')
+
+    @msg.command()
+    @basedbot.has_permissions("msg.caption")
+    async def caption(self, ctx, shorthand, *, caption):
+        """Caption a meme"""
+
+        with self.bot.db.get(ctx.guild.id) as db:
+            result = db.execute("""SELECT name, content, allow_memegen FROM msg
+                                   WHERE allow_memegen AND (name = ? OR name = ?)""",
+                                (shorthand.lower(), "-" + shorthand.lower())).fetchall()
+
+        if len(result) <= 0:
+            await ctx.send("Shorthand not found or captioning has not been enabled for the chosen shorthand.")
+            return
+
+        shorthand_url = result[0][1]
+        if not _is_url(shorthand_url):
+            await ctx.send("Shorthand text must be a valid URL to enable meme generation.")
+            return
+
+        safe_caption = await commands.clean_content(
+            fix_channel_mentions=True, use_nicknames=True
+        ).convert(ctx, caption)
+        safe_caption = _memegen_escape_text(safe_caption)
+        safe_caption = quote(safe_caption, safe='')
+        quoted_url = quote(shorthand_url, safe='')
+
+        text = f"https://api.memegen.link/images/custom/_/{safe_caption}.gif?background={quoted_url}"
+
+        await ctx.send(text)
 
     @msg.command()
     @basedbot.has_permissions("msg.delete")
@@ -82,6 +177,9 @@ def setup(bot):
     bot.perm.register('msg.list',
                       base=True,
                       pretty_name="List shorthands (msg)")
+    bot.perm.register('msg.caption',
+                      base=True,
+                      pretty_name="Caption images (msg)")
     bot.perm.register('msg.set',
                       base="administrator",
                       pretty_name="Create/Update shorthands (msg)")
